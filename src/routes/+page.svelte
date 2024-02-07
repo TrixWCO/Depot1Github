ae9c250b4101cb814e6f5ec61d663a9dd56d36fd
TrixWCO <maleke.ibnmohamed@gmail.com>
2024-02-07 12:50:22 +0300

    Svelte image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,14 @@
 </script>
 
 <div class="container mx-auto space-y-8 p-8">
-	<h1 class="h1 text-center">!</h1>
+	<h1 class="h1 text-center text-amber-400">Page principal</h1>
 </div>
+<div class='flex justify-center'>
+<img src="/favicon.png"  alt="">
+</div>
+
+<style>
+
+
+
+</style>
